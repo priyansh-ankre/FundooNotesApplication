493b9fbf0e8e7e54c4e394ad30b1a1e0e3a9f4fa
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-01 18:24:21 +0530

    [Priyansh]Refactor:proper indentation of login ts

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -13,8 +13,10 @@ export class LoginComponent implements OnInit {
   hide = true;
   form: FormGroup;
 
-  constructor(private fb: FormBuilder,
-    private service: UserService) {
+  constructor(
+    private fb: FormBuilder,
+    private service: UserService
+  ) {
     this.form = this.fb.group({
       email: ['', [Validators.email, Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.pattern("^([a-zA-Z0-9])*[!@#$%^&*]{1}([a-zA-Z0-9])*$"), Validators.required]],
