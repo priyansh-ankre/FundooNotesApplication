7d6686331943c9f6626e0a7f65388a774163b765
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-01 18:39:52 +0530

    [Priyansh]Refactor:removed redundant url from user service

--- src/app/services/userservices/user.service.ts
+++ src/app/services/userservices/user.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpService } from '../httpservices/http.service';
@@ -9,16 +10,16 @@ import { LoginModel } from 'src/app/model/login-model/login-model';
 })
 export class UserService {
 
-  registerUrl="http://fundoonotes.incubation.bridgelabz.com/api/user/registerPushToken";
-  loginUrl="http://fundoonotes.incubation.bridgelabz.com/api/user/login";
+  registerUrl = environment.baseUrl + "user/registerPushToken";
+  loginUrl = environment.baseUrl + "user/login";
 
-  constructor(private httpService:HttpService) { }
+  constructor(private httpService: HttpService) { }
 
-  public doRegistration(registrationInterface:RegistrationModel):Observable<any>{
-    return this.httpService.post(this.registerUrl,registrationInterface);
+  public doRegistration(registrationInterface: RegistrationModel): Observable<any> {
+    return this.httpService.post(this.registerUrl, registrationInterface);
   }
 
-  public doLogin(loginInterface:LoginModel):Observable<any>{
-    return this.httpService.post(this.loginUrl,loginInterface);
+  public doLogin(loginInterface: LoginModel): Observable<any> {
+    return this.httpService.post(this.loginUrl, loginInterface);
   }
 }
