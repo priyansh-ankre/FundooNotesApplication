38d0947045443b05cc513d4bdc5d392327ce4ade
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-01 18:57:41 +0530

    [Priyansh]Add:create-note UI and deleted in dashboard

--- src/app/component/create-note/create-note.component.ts
+++ src/app/component/create-note/create-note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NoteModel } from 'src/app/model/note-model/note-model';
+import { NoteService } from 'src/app/services/noteservices/note.service';
 
 @Component({
   selector: 'app-create-note',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNoteComponent implements OnInit {
 
-  constructor() { }
+  switchBox = false;
+  form: FormGroup;
+
+  constructor(
+    private service: NoteService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      title: '',
+      description: '',
+      isPinned: false,
+      color: '',
+      isArchived: false,
+      labelIdList: [],
+      reminder: Date,
+      collaberators: []
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  clickBox() {
+    this.switchBox = !this.switchBox;
+    this.addNote();
+  }
+
+  addNote() {
+    this.service.addNote(new NoteModel(
+      this.form.get('title').value,
+      this.form.get('description').value,
+      this.form.get('isPinned').value,
+      this.form.get('color').value,
+      this.form.get('isArchived').value,
+      this.form.get('labelIdList').value,
+      this.form.get('reminder').value,
+      this.form.get('collaberators').value
+    ))
   }
 
 }
