351614afc44f6a6abb797ae4e40321156256ecfc
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-09 10:25:26 +0530

    [Priyansh]Add:child routing for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,8 @@
+import { RemindersComponent } from './component/reminders/reminders.component';
+import { TrashComponent } from './component/trash/trash.component';
+import { LabelComponent } from './component/label/label.component';
+import { ArchiveComponent } from './component/archive/archive.component';
+import { NoteComponent } from './component/note/note.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardsComponent } from './component/dashboards/dashboards.component';
@@ -7,7 +12,16 @@ import { RegistrationComponent } from './component/registration/registration.com
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: 'dashboard', component: DashboardsComponent }
+  {
+    path: 'dashboard', component: DashboardsComponent,
+    children: [
+      { path: '', component: NoteComponent },
+      {path:'archive',component:ArchiveComponent},
+      {path: 'label',component:LabelComponent},
+      {path:'trash',component:TrashComponent},
+      {path:'reminders',component:RemindersComponent}
+    ]
+  }
 ];
 
 @NgModule({
