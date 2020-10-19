9de7d2e4473eef28bf1f6cb88eb01f3abdb0f921
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-19 19:20:40 +0530

    [Priyansh]Add:functionality for grid-list view button

--- src/app/component/display-note/display-note.component.ts
+++ src/app/component/display-note/display-note.component.ts
@@ -13,12 +13,14 @@ export class DisplayNoteComponent {
   @Input() noteData: any[];
   @Output() getNotes:EventEmitter<any>=new EventEmitter();
   searchItem:string;
+  view:string;
 
   constructor(
     public dialog: MatDialog,
     private dashboard:DashboardsComponent
   ) {
     this.searchItem=this.dashboard.search.value;
+    
   }
 
   ngOnInit(): void {
@@ -34,5 +36,44 @@ export class DisplayNoteComponent {
     })
   }
   
+  changeView(){
+    this.view=this.dashboard.view.valueOf();
+  }
+
+  changeLayout(){
+    this.changeView();
+    let cssClass;
+    if(this.view=="grid_on"){
+      cssClass={
+        'mat-display-container-grid':false,
+        'mat-display-container-list':true
+      }
+    }
+    else{
+      cssClass={
+        'mat-display-container-grid':true,
+        'mat-display-container-list':false
+      }
+    }
+    return cssClass;
+  }
+
+  changeNoteCardCss(){
+    this.changeView();
+    let cssClass;
+    if(this.view=="grid_on"){
+      cssClass={
+        'mat-display-grid':false,
+        'mat-display-list':true
+      }
+    }
+    else{
+      cssClass={
+        'mat-display-grid':true,
+        'mat-display-list':false
+      }
+    }
+    return cssClass;
+  }
   
 }
