1f55b8a99f46b7945b8ca1ebdceb82ededa8fb6b
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-20 15:12:49 +0530

    [Priyansh]Add:edit-label component in component folder

---
+++ src/app/component/edit-label/edit-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-label',
+  templateUrl: './edit-label.component.html',
+  styleUrls: ['./edit-label.component.css']
+})
+export class EditLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
