9d1df7ae485315329f5206c2c28ed200204185af
priyansh-ankre <bod123priyansh@gmail.com>
2020-10-20 21:51:44 +0530

    [Priyansh]Add:addLabel method in label

--- src/app/component/label/label.component.ts
+++ src/app/component/label/label.component.ts
@@ -1,3 +1,5 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoteService } from 'src/app/services/noteservices/note.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, OnInit, Inject } from '@angular/core';
 
@@ -10,10 +12,23 @@ export class LabelComponent implements OnInit {
 
   constructor(
     public dialogRef:MatDialogRef<LabelComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any
+    @Inject(MAT_DIALOG_DATA) public data:any,
+    private noteService:NoteService,
+    private snackBar:MatSnackBar
   ) { }
 
   ngOnInit(): void {
   }
 
+  addLabel(note){
+    this.noteService.addLabel(note)
+    .subscribe((response)=>{
+      console.log('label response',response);
+      this.dialogRef.close();
+    },(error)=>{
+      this.snackBar.open('error occured',error,{
+        duration:3000
+      })
+    })
+  }
 }
